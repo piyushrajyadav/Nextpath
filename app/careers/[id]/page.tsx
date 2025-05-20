5f1d74035dc18c17e5f6bae0a9daea4fe172e027
Piyush Yadav <158178653+piyushrajyadav@users.noreply.github.com>
2025-05-20 11:01:37 +0530

    Update page.tsx

--- app/careers/[id]/page.tsx
+++ app/careers/[id]/page.tsx
@@ -1,17 +1,18 @@
 import CareerDetailClient from '@/app/careers/[id]/CareerDetailClient';
 import { Metadata } from 'next';
 
-type Props = {
-  params: { id: string }
-  searchParams: { [key: string]: string | string[] | undefined }
+interface PageProps {
+  params: {
+    id: string;
+  };
 }
 
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
+export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
   return {
     title: `Career Details - ${params.id}`,
   }
 }
 
-export default function CareerDetailPage(props: Props) {
-  return <CareerDetailClient id={props.params.id} />;
+export default function CareerDetailPage({ params }: PageProps) {
+  return <CareerDetailClient id={params.id} />;
 } 
